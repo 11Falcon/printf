e7ded0132f08a72fdcb68c693583ba5e0b1ed506
soufiane <soufian9mkns@gmail.com>
2023-08-19 15:09:46 -0700

    Freedom

--- printf.c
+++ printf.c
@@ -13,8 +13,8 @@ int _printf(const char *format, ...)
 
 	if (format == NULL || (format[0] == '%' && !format[1]))
 		return (-1);
-	if (format[0] == '%' && format[1] == ' ' && !format[2])
-	       return (-1);	
+	if ((format[0] == '%' && format[1] == ' ') && (!format[2]))
+		return (-1);
 	va_start(ap, format);
 	while (*format)
 	{
