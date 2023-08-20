26b679e3cd49c141aece0846e9dad4fec509b5df
soufiane <soufian9mkns@gmail.com>
2023-08-19 23:33:45 -0700

    Freedom

--- printf.c
+++ printf.c
@@ -14,7 +14,6 @@ int _printf(const char *format, ...)
 	if (format == NULL || (format[0] == '%' && !format[1]))
 	{
 		write(1,"(null)",6);
-		return (-1);
 	}
 	if ((format[0] == '%' && format[1] == ' ') && (!format[2]))
 		return (-1);
