073d58d5c4d233a6d286b73d610fb7ff832a1c85
soufiane <soufian9mkns@gmail.com>
2023-08-22 03:19:02 -0700

    Freedom

--- print_b.c
+++ print_b.c
@@ -11,49 +11,31 @@ int print_b(va_list ap, int falcon)
 {
 	int result[32];
 	long int d = va_arg(ap, long int);
-	int positive, i;
+	int i;
 	long int d_im = d;
 
-	if (d < 0)
-	{
-		d_im = -d_im;
-		positive = 0;
-	}
-	else
-		positive = 1;
 	for (i = 31; i >= 0; i--)
 	{
 		long int div = 1 << i;
 
-		if (positive == 1)
+		if ((d_im / div) >= 1)
 		{
-			if ((d_im / div) >= 1)
-			{
-				d_im -= div;
-				result[i] = 1;
-			}
-			else
-				result[i] = 0;
+			d_im -= div;
+			result[i] = 1;
 		}
 		else
-		{
-			if ((d_im / div) >= 1)
-			{
-				d_im -= div;
-				result[i] = 0;
-			}
-			else
-				result[i] = 1;
-		}
+			result[i] = 0;
 	}
 	i = 31;
 	while ((i >= 0) && result[i] == 0)
 		i--;
 	if (i < 0)
 	{
-		char c = '0';
+		char c = '%';
 		write(1, &c, 1);
-		falcon++;
+		c = 'b';
+		write(1, &c, 1);
+		falcon += 2;
 	}
 	else
 	{
