fb6f723012c6752826ccbde10c69a70e9af561ef
soufiane <soufian9mkns@gmail.com>
2023-08-22 18:39:58 -0700

    Falcon

--- main.c
+++ main.c
@@ -1,5 +1,3 @@
-#include <limits.h>
-#include <stdio.h>
 #include "main.h"
 
 /**
@@ -9,18 +7,6 @@
  */
 int main(void)
 {
-	unsigned int ui;
-
-	ui = (unsigned int)INT_MAX + 1024;
-
-	_printf("%u\n", 98);
-	printf("%u\n", 98);
-	_printf("Unsigned:[%u]\n", ui);
-	printf("Unsigned:[%u]\n", ui);
-	_printf("Unsigned octal:[%o]\n", ui);
-	printf("Unsigned octal:[%o]\n", ui);
-	_printf("Unsigned hexadecimal:[%x, %X]\n", ui, ui);
-	printf("Unsigned hexadecimal:[%x, %X]\n", ui, ui);
-
-	return (0);
+    _printf("%b\n", 98);
+    return (0);
 }
--- print_glob.c
+++ print_glob.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include <stdarg.h>
 /**
  * print_glob - global function
  * @format: input
