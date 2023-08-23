58cb956401b8701391a926fe0f50f4e45ce14066
soufiane <soufian9mkns@gmail.com>
2023-08-22 18:29:11 -0700

    Falcon

---
+++ print_h.c
@@ -0,0 +1,57 @@
+#include "main.h"
+#include <unistd.h>
+#include <stdarg.h>
+#include <stdlib.h>
+#include <stdio.h>
+/**
+ * print_o - Entry code
+ * @ap: input
+ * @falcon
+ * Return: int
+ */
+int print_o(va_list ap, int falcon)
+{
+        unsigned int *a = (unsigned int *)calloc(15, sizeof(unsigned int));
+       	unsigned int d_im = va_arg(ap, unsigned int), temp = d_im;
+        char c;
+        int i, num = 0;
+
+	while (temp >0)
+	{
+		temp /= 16;
+		num++;
+	}
+	for (i = num - 1; i >= 0; i--)
+        {
+                unsigned int div = 1;
+		int s = i;
+		while (s > 0)
+		{
+			s--;
+			div *= 16;
+		}
+                if ((d_im / div) >= 1)
+                {
+                        unsigned int rest = d_im / div;
+                        a[i] = rest;
+			d_im -= rest * div;
+                }
+        }
+        for (i = num - 1; (i >= 0) && a[i] == 0;)
+                i--;
+        if (i < 0)
+        {
+                c = '0';
+                write(1, &c, 1);
+                falcon++;
+        }
+        else
+                for (; i >= 0; i--)
+                {
+                        c = a[i] + '0';
+                        write(1, &c, 1);
+                        falcon++;
+                }
+        free(a);
+        return (falcon);
+}
--- print_o.c
+++ print_o.c
@@ -2,56 +2,47 @@
 #include <unistd.h>
 #include <stdarg.h>
 #include <stdlib.h>
-#include <stdio.h>
 /**
  * print_o - Entry code
  * @ap: input
- * @falcon
+ * @falcon: the king
  * Return: int
  */
 int print_o(va_list ap, int falcon)
 {
-        unsigned int *a = (unsigned int *)calloc(15, sizeof(unsigned int));
-       	unsigned int d_im = va_arg(ap, unsigned int), temp = d_im;
-        char c;
-        int i, num = 0;
+	unsigned int *a = (unsigned int *)calloc(32, sizeof(unsigned int));
+	unsigned int d_im = va_arg(ap, unsigned int);
+	char c;
+	int i;
 
-	while (temp >0)
+	for (i = 31; i >= 0; i--)
 	{
-		temp /= 8;
-		num++;
-	}
-	for (i = num - 1; i >= 0; i--)
-        {
-                unsigned int div = 1;
-		int s = i;
-		while (s > 0)
+		int div = 7 << i;
+
+		if ((d_im / div) >= 1)
 		{
-			s--;
-			div *= 8;
+			unsigned int rest = d_im - div;
+
+			d_im -= div;
+			a[i] = rest;
 		}
-                if ((d_im / div) >= 1)
-                {
-                        unsigned int rest = d_im / div;
-                        a[i] = rest;
-			d_im -= rest * div;
-                }
-        }
-        for (i = num - 1; (i >= 0) && a[i] == 0;)
-                i--;
-        if (i < 0)
-        {
-                c = '0';
-                write(1, &c, 1);
-                falcon++;
-        }
-        else
-                for (; i >= 0; i--)
-                {
-                        c = a[i] + '0';
-                        write(1, &c, 1);
-                        falcon++;
-                }
-        free(a);
-        return (falcon);
+	}
+	i = 31;
+	for (i = 31; (i >= 0) && a[i] == 0;)
+		i--;
+	if (i < 0)
+	{
+		c = '0';
+		write(1, &c, 1);
+		falcon++;
+	}
+	else
+		for (; i >= 0; i--)
+		{
+			c = a[i] + '0';
+			write(1, &c, 1);
+			falcon++;
+		}
+	free(a);
+	return (falcon);
 }
